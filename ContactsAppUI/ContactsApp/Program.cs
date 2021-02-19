0d4f18dba155edf99149d89c371e1de0c2733aed
Sevo98 <sevo98.sk@gmail.com>
2021-02-20 02:39:51 +0500

    Добавлен проект логики

--- ContactsAppUI/ContactsApp/Program.cs
+++ ContactsAppUI/ContactsApp/Program.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using ContactsApp;
 
-namespace ContactsAppUI
+namespace ContactsApp
 {
     static class Program
     {
