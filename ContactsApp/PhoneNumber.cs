f8ee8d969265f617cc2252b943ea285920df2bd4
Sevo98 <sevo98.sk@gmail.com>
2021-04-03 02:22:29 +0700

    Create PhoneNumber class.

---
+++ ContactsApp/PhoneNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    public class PhoneNumber
+    {
+        private int _number;
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (!Number.ToString().StartsWith("7"))
+                {
+                    throw new ArgumentException("Номер должен начинаться с 7!");
+                }
+
+                if (Number.ToString().Length != 11)
+                {
+                    throw new ArgumentException("Номер должен состоять из 11 цифр!");
+                }
+
+            }
+        }
+    }
+}
