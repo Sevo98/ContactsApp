307b7906a68210fd77b0543b1158cb2734ffa30a
Sevo98 <sevo98.sk@gmail.com>
2021-04-07 14:28:10 +0700

    Create SeveToFile in ProjectManager

---
+++ ContactsApp/ProjectManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Newtonsoft.Json;
+
+namespace ContactsApp
+{
+    public static class ProjectManager
+    {
+        private static JsonSerializer serializer = new JsonSerializer();
+        private static string _myDocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static void SaveToFile(Project project, string fileName)
+        {
+            using (StreamWriter sw = new StreamWriter(_myDocPath + @fileName))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, project);
+            }
+        }
+
+        public static void LoadFromFile(string fileName)
+        {
+        }
+    }
+}
